75fcf95292379d1154c68e7de41964331b74df35
lihaoyeh <rock20341@gmail.com>
2019-06-24 13:26:19 -0700

    updated package name and fix examples

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
-setup(  name             = 'waverders',
+setup(  name             = 'waverder',
         version          = '0.0.1',
         description      = 'functions for reconstructing and visualizing phase and birefrigence',
         author           = 'Li-Hao Yeh',
-        author_email     = 'lihao.yeh@berkeley.edu',
+        author_email     = 'lihao.yeh@czbiohub.org',
         license          = 'BSD License',
         packages         = find_packages(),
         install_requires = ['numpy', 'matplotlib', 'scipy', 'ipywidgets']
