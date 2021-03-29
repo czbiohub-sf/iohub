4526f054a8b710390ad61e63b63daf97048ec46f
Bryant <bchhun@gmail.com>
2021-03-29 12:29:20 -0700

    notes for writer

--- iohub/writer.py
+++ iohub/writer.py
@@ -13,6 +13,18 @@ use zarr.copy_store to copy memory store to directory store
 
 """
 
+# todo: how should we supply data?  as individual arrays?
+# todo: don't forget that the user should be allowed to supply np arrays and zarr arrays
+# todo: change to persistent arrays
+
+
+# initialize upfront -- with file path and dimensions -- use zeros and replace later
+#       initial dimensions are just (p, t, z, y, x) = (1, 1, 1, 512, 512) ==> (file) 2.0.0._._
+#       Initial chunks are important, must determine this first.
+# as data is reconstructed, replace zeros using same zarr reference.
+#
+
+
 class WaveorderWriter:
     """
     given stokes or physical data, construct a standard hierarchy in zarr for output
@@ -21,10 +33,11 @@ class WaveorderWriter:
     """
     __builder = None
 
-    def __init__(self, datatype: str):
+    def __init__(self, datatype: str, chunksize: tuple):
         """
         datatype is one of "stokes" or "physical"
         :param datatype:
+        :param chunksize:
         """
         if datatype == 'stokes':
             self.__builder = StokesZarr()
@@ -60,35 +73,50 @@ class WaveorderWriter:
 
     def set_physical(self, physical_data: np.ndarray):
         # assign class attribute data
-        # call self.__builder attributes
-        pass
+        # desired keyword args:
+        #   data, channel, chunks, compressor
+        # can be learned from supplied keywords:
+        #   data.shape, chunk compatibility,
+        self.__builder.init_arrays()
 
-    def write_stokes(self):
-        pass
+    # def write_stokes(self):
+    #     pass
+    #
+    # def write_physical(self):
+    #     pass
 
-    def write_physical(self):
+    def write(self):
         pass
 
 
 class Builder:
     # interface for all builders
+
+    # create zarr memory store
     def init_zarr(self): pass
+
+    # create subarrays named after the channel
     def init_arrays(self): pass
+
+    # assign group and array attributes
     def init_meta(self): pass
+
+    # create a directory store and write the memory store to that
     def to_disk(self): pass
 
 
-class StokesZarr(Builder):
-    # define stokes zarr structure here
-    __szarr = None
-
-    def init_zarr(self, **kwargs):
-        self.__szarr = zarr.MemoryStore()
-
-
 class PhysicalZarr(Builder):
-    # define physical zarr structure here
     __pzarr = None
 
     def init_zarr(self, **kwargs):
-        self.__pzarr = zarr.MemoryStore()
+        self.__pzarr = zarr.DirectoryStore()
+
+    def init_arrays(self, **kwargs):
+        pass
+
+
+class StokesZarr(Builder):
+    __szarr = None
+
+    def init_zarr(self, **kwargs):
+        self.__szarr = zarr.DirectoryStore()
