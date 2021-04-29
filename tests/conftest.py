6d71998bfb8b5802c4276920ad3527a222aee63a
Bryant <bchhun@gmail.com>
2021-04-29 13:40:30 -0700

    few ome-tiff tests plus fixed conftests

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,10 +4,32 @@ import shutil
 
 from google_drive_downloader import GoogleDriveDownloader as gdd
 
+MM2GAMMA_OMETIFF_SUBFOLDERS = \
+    {
+        'mm2.0-20201209_4p_2t_5z_1c_512k_1',
+        'mm2.0-20201209_4p_2t_5z_512k_1',
+        'mm2.0-20201209_4p_2t_3c_512k_1',
+        'mm2.0-20201209_4p_5z_3c_512k_1',
+        'mm2.0-20201209_4p_2t_512k_1',
+        'mm2.0-20201209_4p_3c_512k_1',
+        'mm2.0-20201209_4p_5z_512k_1',
+        'mm2.0-20201209_1t_5z_3c_512k_1',
+        'mm2.0-20201209_1t_5z_512k_1',
+        'mm2.0-20201209_2t_3c_512k_1',
+        'mm2.0-20201209_5z_3c_512k_1',
+        'mm2.0-20201209_4p_512k_1',
+        'mm2.0-20201209_2t_512k_1',
+        'mm2.0-20201209_3c_512k_1',
+        'mm2.0-20201209_5z_512k_1',
+        'mm2.0-20201209_100t_5z_3c_512k_1_nopositions',
+        'mm2.0-20201209_4p_20t_5z_3c_512k_1_multipositions',
+    }
+
 
 @pytest.fixture(scope="session")
 def setup_mm2gamma_ome_tiffs():
     temp_folder = os.getcwd() + '/pytest_temp'
+    temp_gamma = os.path.join(temp_folder, 'mm2gamma')
     if not os.path.isdir(temp_folder):
         os.mkdir(temp_folder)
         print("\nsetting up temp folder")
@@ -18,7 +40,7 @@ def setup_mm2gamma_ome_tiffs():
     mm2gamma_ometiffs = '1WSu1CaFQKIipMproxPInYs6ljh17JM7K'
 
     # download files to temp folder
-    output = temp_folder + "/mm2gamma_ometiffs.zip"
+    output = temp_gamma + "/mm2gamma_ometiffs.zip"
     gdd.download_file_from_google_drive(file_id=mm2gamma_ometiffs,
                                         dest_path=output,
                                         unzip=True,
@@ -26,7 +48,7 @@ def setup_mm2gamma_ome_tiffs():
                                         overwrite=True)
 
     # return path to unzipped folder containing test images
-    yield output.strip('.zip')
+    yield os.path.join(temp_gamma, 'ome-tiffs')
 
     # breakdown files
     try:
@@ -34,7 +56,7 @@ def setup_mm2gamma_ome_tiffs():
         os.remove(output)
 
         # remove unzipped folder
-        shutil.rmtree(output.strip('.zip'))
+        shutil.rmtree(os.path.join(temp_gamma, 'ome-tiffs'))
 
         # remove temp folder
         shutil.rmtree(temp_folder)
@@ -45,6 +67,7 @@ def setup_mm2gamma_ome_tiffs():
 @pytest.fixture(scope="session")
 def setup_mm2gamma_singlepage_tiffs():
     temp_folder = os.getcwd() + '/pytest_temp'
+    temp_gamma = os.path.join(temp_folder, 'mm2gamma')
     if not os.path.isdir(temp_folder):
         os.mkdir(temp_folder)
         print("\nsetting up temp folder")
@@ -55,7 +78,7 @@ def setup_mm2gamma_singlepage_tiffs():
     mm2gamma_singlepage_tiffs = '1qQys8r0_HIsVqLrMfUzsndTgTtyPhZes'
 
     # download files to temp folder
-    output = temp_folder + "/mm2gamma_singlepage_tiffs.zip"
+    output = temp_gamma + "/mm2gamma_singlepage_tiffs.zip"
     gdd.download_file_from_google_drive(file_id=mm2gamma_singlepage_tiffs,
                                         dest_path=output,
                                         unzip=True,
@@ -63,7 +86,7 @@ def setup_mm2gamma_singlepage_tiffs():
                                         overwrite=True)
 
     # return path to folder with images
-    yield output.strip('.zip')
+    yield os.path.join(temp_gamma, 'singlepage-tiffs')
 
     # breakdown files
     try:
@@ -71,7 +94,7 @@ def setup_mm2gamma_singlepage_tiffs():
         os.remove(output)
 
         # remove unzipped folder
-        shutil.rmtree(output.strip('.zip'))
+        shutil.rmtree(os.path.join(temp_gamma, 'ome-tiffs'))
 
         # remove temp folder
         shutil.rmtree(temp_folder)
--- tests/reader/test_multipagetiff.py
+++ tests/reader/test_multipagetiff.py
@@ -1,8 +1,62 @@
 import numpy as np
 import pytest
+import os
+import random
 
 from waveorder.io.multipagetiff import MicromanagerOmeTiffReader
 
 
-def test_constructor(setup_mm2):
-    mmr = MicromanagerOmeTiffReader()
+def test_constructor(setup_mm2gamma_ome_tiffs):
+    """
+    test that constructor parses metadata properly
+        no data extraction in this test
+    Parameters
+    ----------
+    setup_mm2gamma_ome_tiffs
+
+    Returns
+    -------
+
+    """
+    join = os.path.join
+    folder = setup_mm2gamma_ome_tiffs
+    subfolders = [f for f in os.listdir(folder) if os.path.isdir(join(folder, f))]
+
+    # choose a specific subfolder
+    one_folder = join(folder, subfolders[0])
+    mmr = MicromanagerOmeTiffReader(one_folder, extract_data=False)
+
+    assert(mmr.mm_meta is not None)
+    assert(mmr.width is not 0)
+    assert(mmr.height is not 0)
+    assert(mmr.frames is not 0)
+    assert(mmr.slices is not 0)
+    assert(mmr.channels is not 0)
+    assert(mmr.master_ome_tiff is not None)
+
+
+def test_output_dims(setup_mm2gamma_ome_tiffs):
+    """
+    test that output dimensions are always (t, c, z, y, x)
+    Parameters
+    ----------
+    setup_mm2gamma_ome_tiffs
+
+    Returns
+    -------
+
+    """
+    join = os.path.join
+    folder = setup_mm2gamma_ome_tiffs
+    subfolders = [f for f in os.listdir(folder) if os.path.isdir(join(folder, f))]
+
+    # choose random subfolder
+    one_folder = join(folder, random.choice(subfolders))
+    mmr = MicromanagerOmeTiffReader(one_folder, extract_data=True)
+
+    assert(mmr.get_zarr(0).shape[0] == mmr.frames)
+    assert(mmr.get_zarr(0).shape[1] == mmr.channels)
+    assert(mmr.get_zarr(0).shape[2] == mmr.slices)
+    assert(mmr.get_zarr(0).shape[3] == mmr.height)
+    assert(mmr.get_zarr(0).shape[4] == mmr.width)
+
