c48dd8bc20289803629d33510cacaaf617efb162
Bryant <bchhun@gmail.com>
2021-08-07 11:35:58 -0700

    bugfix, zarrfiles are directories not files refactor get_zarr, get_array to receive tuples of (position, time) specifically for recOrder

--- iohub/zarrfile.py
+++ iohub/zarrfile.py
@@ -18,13 +18,14 @@ class ZarrReader(ReaderInterface):
     def __init__(self,
                  zarrfile: str):
 
-        if not os.path.isfile(zarrfile):
+        # zarr files (.zarr) are directories
+        if not os.path.isdir(zarrfile):
             raise ValueError("file is not a .zarr file")
-        if not '.zarr' in zarrfile:
-            raise ValueError("file is not a .zarr file")
+        # if not '.zarr' in zarrfile:
+        #     raise ValueError("file is not a .zarr file")
 
         self.zf = zarrfile
-        self.z = zarr.open(self.zf, 'r')
+        self.z = zarr.open(self.zf, 'r', chunks=(1, 1, 1, 1, 2048, 2048))
 
         # structure of zarr array
         (self.pos,
@@ -34,18 +35,21 @@ class ZarrReader(ReaderInterface):
          self.height,
          self.width) = self.z.shape
 
-        self.positions = {}
-        for p in range(self.pos):
-            self.positions[p] = self.z[p]
+        # self.positions = {}
+        # for p in range(self.pos):
+        #     self.positions[p] = self.z[p]
 
-    def get_zarr(self, position: int) -> zarr.array:
-        return self.positions[position]
+    def get_zarr(self, pt: tuple) -> zarr.array:
+        # return self.positions[position]
+        return self.z[pt[0], pt[1]].astype(np.uint16)
 
-    def get_array(self, position: int) -> np.ndarray:
-        return np.array(self.positions[position])
+    def get_array(self, pt: tuple) -> np.ndarray:
+        # return np.array(self.positions[position])
+        return np.array(self.z[pt[0], pt[1]]).astype(np.uint16)
 
     def get_num_positions(self) -> int:
-        return len(self.positions)
+        # return len(self.positions)
+        return len(self.z)
 
     @property
     def shape(self):
