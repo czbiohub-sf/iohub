03d10ca3a0aa4d6b04552f92b6b018a22e1d5473
camfoltz <camfoltz2@gmail.com>
2021-12-14 14:19:26 -0800

    get ready for pypi release

--- setup.py
+++ setup.py
@@ -1,11 +1,42 @@
 from setuptools import setup, find_packages
 
 setup(  name             = 'waveorder',
-        version          = '0.0.1',
-        description      = 'functions for reconstructing and visualizing phase and birefrigence',
+        version          = '1.0.0-beta',
+        description      = 'wave optical simulations and deconvolution of optical properties',
         author           = 'Li-Hao Yeh',
         author_email     = 'lihao.yeh@czbiohub.org',
-        license          = 'BSD License',
+        url              = 'https://github.com/mehta-lab/waveorder/',
+        license          = 'BSD License (Chan Zuckerberg Biohub Software License)',
+        license_file     = 'LICENSE.txt',
+        long_description = 'file: README.md',
+        long_description_content_type = 'text/markdown',
         packages         = find_packages(),
-        install_requires = ['numpy', 'matplotlib', 'scipy', 'ipywidgets']
+        python_requies   = '==3.7',
+        install_requires = [
+                'numpy>=1.17.4',
+                'matplotlib>=3.1.1',
+                'scipy>=1.3.0',
+                'pywavelets>=1.1.1',
+                'ipywidgets>=7.5.1',
+                'opencv-python>=3.4.1',
+                'tifffile>=2020.11.26',
+                'zarr>=2.6.1',
+                'natsort>=7.1.1'
+        ],
+        classifiers = [
+                'Development Status :: 4 - Beta',
+                'Intended Audience :: Science/Research',
+                'License :: OSI Approved :: BSD License',
+                'Programming Language :: Python :: 3 :: Only',
+                'Programming Language :: Python :: 3.7',
+                'Topic :: Scientific/Engineering',
+                'Topic :: Scientific/Engineering :: Image Processing',
+                'Topic :: Scientific/Engineering :: Visualization',
+                'Topic :: Scientific/Engineering :: Information Analysis',
+                'Topic :: Scientific/Engineering :: Bio-Informatics',
+                'Operating System :: Microsoft :: Windows',
+                'Operating System :: POSIX',
+                'Operating System :: Unix',
+                'Operating System :: MacOS',
+        ]
      )
