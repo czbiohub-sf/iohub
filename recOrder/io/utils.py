6f0c537fc6e089c12697786448f7ace875dda35d
Talon Chandler <talonchandler@gmail.com>
2022-10-06 10:07:17 -0700

    Merge pull request #219 from mehta-lab/0.2.0-integration
    0.2.0 integration PR

--- recOrder/io/utils.py
+++ recOrder/io/utils.py
@@ -30,16 +30,22 @@ def extract_reconstruction_parameters(reconstructor, magnification=None):
         ps = ps * magnification if magnification else ps
 
     if isinstance(reconstructor, waveorder_microscopy):
-        attr_dict = {'phase_dimension': reconstructor.phase_deconv,
-                     'wavelength (nm)': np.round(reconstructor.lambda_illu * 1000 * reconstructor.n_media,1),
-                     'pad_z': reconstructor.pad_z,
-                     'n_objective_media': reconstructor.n_media,
-                     'bg_correction_option': reconstructor.bg_option,
-                     'objective_NA': reconstructor.NA_obj * reconstructor.n_media,
-                     'condenser_NA': reconstructor.NA_illu * reconstructor.n_media,
-                     'magnification': magnification,
-                     'swing': reconstructor.chi if reconstructor.N_channel == 4 else reconstructor.chi / 2 / np.pi,
-                     'pixel_size': ps}
+        attr_dict = {
+            "phase_dimension": reconstructor.phase_deconv,
+            "wavelength (nm)": np.round(
+                reconstructor.lambda_illu * 1000 * reconstructor.n_media, 1
+            ),
+            "pad_z": reconstructor.pad_z,
+            "n_objective_media": reconstructor.n_media,
+            "bg_correction_option": reconstructor.bg_option,
+            "objective_NA": reconstructor.NA_obj * reconstructor.n_media,
+            "condenser_NA": reconstructor.NA_illu * reconstructor.n_media,
+            "magnification": magnification,
+            "swing": reconstructor.chi
+            if reconstructor.N_channel == 4
+            else reconstructor.chi / 2 / np.pi,
+            "pixel_size": ps,
+        }
 
     else:
         attr_dict = dict()
@@ -61,11 +67,16 @@ def load_bg(bg_path, height, width, ROI=None):
     bg_data   : (ndarray) Array of background data w/ dimensions (N_channel, Y, X)
     """
 
-    bg_paths = glob.glob(os.path.join(bg_path, '*.tif'))
+    bg_paths = glob.glob(os.path.join(bg_path, "*.tif"))
     bg_paths.sort()
-    
+
     # Backwards compatibility warning
-    if ROI is not None and ROI != (0, 0, width, height): # TODO: Remove for 1.0.0
+    if ROI is not None and ROI != (
+        0,
+        0,
+        width,
+        height,
+    ):  # TODO: Remove for 1.0.0
         warning_msg = """
         Earlier versions of recOrder (0.1.2 and earlier) would have averaged over the background ROI. 
         This behavior is now considered a bug, and future versions of recOrder (0.2.0 and later) 
@@ -73,19 +84,19 @@ def load_bg(bg_path, height, width, ROI=None):
         """
         logging.warning(warning_msg)
 
-    # Load background images 
+    # Load background images
     bg_img_list = []
     for bg_path in bg_paths:
         bg_img_list.append(tiff.imread(bg_path))
-    bg_img_arr = np.array(bg_img_list) # CYX
+    bg_img_arr = np.array(bg_img_list)  # CYX
 
     # Error if shapes do not match
     # TODO: 1.0.0 move these validation check to waveorder's Polscope_bg_correction
     if bg_img_arr.shape[1:] != (height, width):
         error_msg = "The background image has a different X/Y size than the acquired image."
         raise ValueError(error_msg)
-        
-    return bg_img_arr # CYX
+
+    return bg_img_arr  # CYX
 
 
 def create_grid_from_coordinates(xy_coords, rows, columns):
@@ -116,7 +127,7 @@ def create_grid_from_coordinates(xy_coords, rows, columns):
 
     # reshape XY coordinates into their proper 2D shape
     grid = np.reshape(coords_list, (rows, columns, 2))
-    pos_index_grid = np.zeros((rows,columns), 'uint16')
+    pos_index_grid = np.zeros((rows, columns), "uint16")
     keys = list(coords.keys())
     vals = list(coords.values())
 
@@ -128,9 +139,11 @@ def create_grid_from_coordinates(xy_coords, rows, columns):
 
     return pos_index_grid
 
+
 class MockEmitter:
     def emit(self, value):
         pass
+
 
 def get_unimodal_threshold(input_image):
     """Determines optimal unimodal threshold
@@ -144,7 +157,7 @@ def get_unimodal_threshold(input_image):
     hist_counts, bin_edges = np.histogram(
         input_image,
         bins=256,
-        range=(input_image.min(), np.percentile(input_image, 99.5))
+        range=(input_image.min(), np.percentile(input_image, 99.5)),
     )
     bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
 
@@ -170,13 +183,13 @@ def get_unimodal_threshold(input_image):
         if per_dist > max_dist:
             best_threshold = x0
             max_dist = per_dist
-    assert best_threshold > -np.inf, 'Error in unimodal thresholding'
+    assert best_threshold > -np.inf, "Error in unimodal thresholding"
     return best_threshold
 
 
 def ram_message():
     """
-    Determine if the system's RAM capacity is sufficient for running reconstruction. 
+    Determine if the system's RAM capacity is sufficient for running reconstruction.
     The message should be treated as a warning if the RAM detected is less than 32 GB.
 
     Returns
@@ -188,17 +201,18 @@ def ram_message():
     is_warning = gb_available < 32
 
     if is_warning:
-        message = ' \n'.join(textwrap.wrap(
-               f'recOrder reconstructions often require more than the {gb_available:.1f} ' \
-               f'GB of RAM that this computer is equipped with. We recommend starting with reconstructions of small ' \
-               f'volumes ~1000 x 1000 x 10 and working up to larger volumes while monitoring your RAM usage with '
-               f'Task Manager or htop.',
-        ))
+        message = " \n".join(
+            textwrap.wrap(
+                f"recOrder reconstructions often require more than the {gb_available:.1f} "
+                f"GB of RAM that this computer is equipped with. We recommend starting with reconstructions of small "
+                f"volumes ~1000 x 1000 x 10 and working up to larger volumes while monitoring your RAM usage with "
+                f"Task Manager or htop.",
+            )
+        )
     else:
-        message = f'{gb_available:.1f} GB of RAM is available.'
-    
-    return (is_warning, message)
+        message = f"{gb_available:.1f} GB of RAM is available."
 
+    return (is_warning, message)
 
 
 def rec_bkg_to_wo_bkg(recorder_option) -> str:
@@ -214,12 +228,15 @@ def rec_bkg_to_wo_bkg(recorder_option) -> str:
     waveorder_option
 
     """
-    if recorder_option == 'local_fit+':
-        return 'local_fit'
+    if recorder_option == "local_fit+":
+        return "local_fit"
     else:
         return recorder_option
 
-def generic_hsv_overlay(H, S, V, H_scale=None, S_scale=None, V_scale=None, mode='2D'):
+
+def generic_hsv_overlay(
+    H, S, V, H_scale=None, S_scale=None, V_scale=None, mode="2D"
+):
     """
     Generates a generic HSV overlay in either 2D or 3D
 
@@ -241,9 +258,10 @@ def generic_hsv_overlay(H, S, V, H_scale=None, S_scale=None, V_scale=None, mode=
 
     if H.shape != S.shape or H.shape != S.shape or S.shape != V.shape:
         raise ValueError(
-            f'Channel shapes do not match: {H.shape} vs. {S.shape} vs. {V.shape}')
+            f"Channel shapes do not match: {H.shape} vs. {S.shape} vs. {V.shape}"
+        )
 
-    if mode == '3D':
+    if mode == "3D":
         overlay_final = np.zeros((H.shape[0], H.shape[1], H.shape[2], 3))
         slices = H.shape[0]
     else:
@@ -261,10 +279,10 @@ def generic_hsv_overlay(H, S, V, H_scale=None, S_scale=None, V_scale=None, mode=
         hsv = np.transpose(np.stack([H_, S_, V_]), (1, 2, 0))
         overlay_final[i] = hsv_to_rgb(hsv)
 
-    return overlay_final[0] if mode == '2D' else overlay_final
+    return overlay_final[0] if mode == "2D" else overlay_final
 
 
-def ret_ori_overlay(retardance, orientation, scale, mode='2D', cmap='JCh'):
+def ret_ori_overlay(retardance, orientation, ret_max = 10, mode="2D", cmap="JCh"):
     """
     This function will create an overlay of retardance and orientation with two different colormap options.
     HSV is the standard Hue, Saturation, Value colormap while JCh is a similar colormap but is perceptually uniform.
@@ -273,7 +291,7 @@ def ret_ori_overlay(retardance, orientation, scale, mode='2D', cmap='JCh'):
     ----------
     retardance:             (nd-array) retardance array of shape (N, Y, X) or (Y, X) in nanometers
     orientation:            (nd-array) orientation array of shape (N, Y, X) or (Y, X) in radian [0, pi]
-    scale:                  (tuple) interpolation scale to use for retardance.  Typically use adjusted contrast limits
+    ret_max:                (float) maximum displayed retardance.  Typically use adjusted contrast limits.
     mode:                   (str) '2D' or '3D'
     cmap:                   (str) 'JCh' or 'HSV'
 
@@ -283,30 +301,44 @@ def ret_ori_overlay(retardance, orientation, scale, mode='2D', cmap='JCh'):
 
     """
 
-    orientation = orientation * 180 / np.pi
-    levels = 8
-    ret_scale = (0, 10)
+    orientation = np.copy(orientation) * 180 / np.pi
     noise_level = 1
 
     if retardance.shape != orientation.shape:
         raise ValueError(
-            f'Retardance and Orientation shapes do not match: {retardance.shape} vs. {orientation.shape}')
+            f"Retardance and Orientation shapes do not match: {retardance.shape} vs. {orientation.shape}"
+        )
 
-    if mode == '3D':
-        overlay_final = np.zeros((retardance.shape[0], retardance.shape[1], retardance.shape[2], 3))
+    if mode == "3D":
+        overlay_final = np.zeros(
+            (retardance.shape[0], retardance.shape[1], retardance.shape[2], 3)
+        )
         slices = retardance.shape[0]
     else:
-        overlay_final = np.zeros((1, retardance.shape[-2], retardance.shape[-1], 3))
+        overlay_final = np.zeros(
+            (1, retardance.shape[-2], retardance.shape[-1], 3)
+        )
         orientation = np.expand_dims(orientation, axis=0)
         retardance = np.expand_dims(retardance, axis=0)
         slices = 1
 
     for i in range(slices):
-        ret_ = np.interp(retardance[i], ret_scale, scale)
-        ori_binned = np.round(orientation[i] / 180 * levels + 0.5) / levels - 1 / levels
-        ori_ = np.interp(ori_binned, (0, 1), (0, 360))
+        ret_ = np.copy(
+            retardance[i]
+        )  # copy to avoid modifying original array.
+        ret_[ret_ > ret_max] = ret_max  # clip retardance to specified max.
 
-        if cmap == 'JCh':
+        # FIX ME: this binning code leads to artifacts.
+        # levels = 32
+        # ori_binned = (
+        #     np.round(orientation[i] / 180 * levels + 0.5) / levels - 1 / levels
+        # ) # bin orientation into 32 levels.
+        # ori_ = np.interp(ori_binned, (0, 1), (0, 360))
+
+        ori_ = orientation[i] * (
+            360.0 / 180.0
+        )  # convert 180 degree range into 360 to match periodicity of hue.
+        if cmap == "JCh":
             J = ret_
             C = np.ones_like(J) * 60
             C[retardance[i] < noise_level] = 0
@@ -319,12 +351,20 @@ def ret_ori_overlay(retardance, orientation, scale, mode='2D', cmap='JCh'):
             JCh_rgb[JCh_rgb > 1] = 1
 
             overlay_final[i] = JCh_rgb
-        elif cmap == 'HSV':
-            I_hsv = np.transpose(np.stack([ori_binned, np.ones_like(ori_binned),
-                                           np.minimum(1, ret_ / np.max(ret_))]), (1, 2, 0))
+        elif cmap == "HSV":
+            I_hsv = np.transpose(
+                np.stack(
+                    [
+                        ori_ / 360,
+                        np.ones_like(ori_),
+                        np.minimum(1, ret_ / np.max(ret_)),
+                    ]
+                ),
+                (1, 2, 0),
+            )
             overlay_final[i] = hsv_to_rgb(I_hsv)
 
         else:
-            raise ValueError(f'Colormap {cmap} not understood')
+            raise ValueError(f"Colormap {cmap} not understood")
 
-    return overlay_final[0] if mode == '2D' else overlay_final
+    return overlay_final[0] if mode == "2D" else overlay_final
