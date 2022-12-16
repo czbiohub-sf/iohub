2533a9f760c79df697bf7f0145d1d60435cb5056
Ziwen Liu <67518483+ziw-liu@users.noreply.github.com>
2022-12-16 10:52:12 -0800

    move channel display settings to a separate module

--- iohub/writer.py
+++ iohub/writer.py
@@ -1,68 +1,17 @@
-import os, logging
+import os
 from numcodecs import Blosc
-import numpy as np
 import zarr
 from ome_zarr.reader import Reader
 from ome_zarr.format import format_from_version
 
 from iohub.ngff_meta import *
+from iohub.lf_utils import channel_display_settings
 
 from typing import TYPE_CHECKING, Union, Tuple, List, Dict, Literal
 from numpy.typing import NDArray, DTypeLike
 
 if TYPE_CHECKING:
     from _typeshed import StrOrBytesPath
-
-
-def _channel_display_settings(
-    chan_name: str,
-    clim: Tuple[float, float, float, float] = None,
-    first_chan: bool = False,
-):
-    """This will create a dictionary used for OME-zarr metadata.  Allows custom contrast limits and channel
-    names for display. Defaults everything to grayscale.
-
-    Parameters
-    ----------
-    chan_name : str
-        Desired name of the channel for display
-    clim : Tuple[float, float, float, float], optional
-        Contrast limits (start, end, min, max)
-    first_chan : bool, optional
-        Whether or not this is the first channel of the dataset (display will be set to active), by default False
-
-    Returns
-    -------
-    dict
-        Display settings adherent to ome-zarr standards
-    """
-    U16_FMAX = float(np.iinfo(np.uint16).max)
-    channel_settings = {
-        "Retardance": (0.0, 100.0, 0.0, 1000.0),
-        "Orientation": (0.0, np.pi, 0.0, np.pi),
-        "Phase3D": (-0.2, 0.2, -10, 10),
-        "BF": (0.0, 5.0, 0.0, U16_FMAX),
-        "S0": (0.0, 1.0, 0.0, U16_FMAX),
-        "S1": (-0.5, 0.5, -10.0, 10.0),
-        "S2": (-0.5, 0.5, -10.0, 10.0),
-        "S3": (-1.0, 1.0, -10.0, -10.0),
-        "Other": (0, U16_FMAX, 0.0, U16_FMAX),
-    }
-    if not clim:
-        if chan_name in channel_settings.keys():
-            clim = channel_settings[chan_name]
-        else:
-            clim = channel_settings["Other"]
-    window = WindowDict(start=clim[0], end=clim[1], min=clim[2], max=clim[3])
-    return ChannelMeta(
-        active=first_chan,
-        coefficient=1.0,
-        color="FFFFFF",
-        family="linear",
-        inverted=False,
-        label=chan_name,
-        window=window,
-    )
 
 
 class OMEZarrWriter:
@@ -296,7 +245,7 @@ class OMEZarrWriter:
             if i == 0:
                 first_chan = True
             channels.append(
-                _channel_display_settings(
+                channel_display_settings(
                     channel_name, clim=clim, first_chan=first_chan
                 )
             )
