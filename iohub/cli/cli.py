4bf80abdc478e0f34abf37db3496c39ec261aae1
AhmetCanSolak <asolak14@ku.edu.tr>
2023-01-18 09:03:52 -0800

    initial commit

---
+++ iohub/cli/cli.py
@@ -0,0 +1,60 @@
+import sys
+
+import click
+
+from iohub._version import __version__
+
+VERSION = __version__
+
+
+@click.version_option(version=VERSION)
+def cli():
+    """aydin cli
+
+    Parameters
+    ----------
+    ctx : click.Context
+
+    """
+    sys._excepthook = sys.excepthook
+
+    def exception_hook(exctype, value, traceback):
+        print(exctype, value, traceback)
+        sys._excepthook(exctype, value, traceback)
+        sys.exit(1)
+
+    sys.excepthook = exception_hook
+
+    pass
+
+
+@cli.command()
+@click.argument('files', nargs=-1)
+def info(files):
+    """info command
+
+    Parameters
+    ----------
+    files
+
+    """
+    raise NotImplementedError()
+
+
+@cli.command()
+@click.argument('files', nargs=-1)
+@click.option('-tf', '--target-format', default='', type=str)
+def convert(files, **kwargs):
+    """convert command
+
+    Parameters
+    ----------
+    files
+    kwargs
+
+    """
+    raise NotImplementedError()
+
+
+if __name__ == '__main__':
+    cli()
