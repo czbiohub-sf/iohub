54fde7257a98d8548611709b521f1b92547c544b
Talon Chandler <talonchandler@gmail.com>
2023-02-07 08:34:29 -0800

    Remove

--- iohub/cli/cli.py
+++ iohub/cli/cli.py
@@ -3,8 +3,7 @@ import sys
 import click
 
 from iohub._version import __version__
-from iohub import WaveorderReader
-from iohub.zarr_converter import ZarrConverter
+from iohub.reader import WaveorderReader
 
 VERSION = __version__
 
@@ -34,54 +33,3 @@ def print_reader_info(reader):
     print(f"Channel names:\t {reader.channel_names}")
     print(f"Z step (um):\t {reader.z_step_size}")
     print("")
-
-
-@cli.command()
-@click.help_option("-h", "--help")
-@click.option(
-    "--input",
-    "-i",
-    required=True,
-    type=click.Path(exists=True),
-    help="path to the raw data folder containing ome.tifs",
-)
-@click.option(
-    "--output",
-    "-o",
-    required=True,
-    type=str,
-    help="full path to save the zarr store (../../Experiment.zarr)",
-)
-@click.option(
-    "--data_type",
-    required=False,
-    type=str,
-    help='Data type, "ometiff", "upti", "zarr"',
-)
-@click.option(
-    "--replace_pos_name",
-    required=False,
-    type=bool,
-    help="whether or not to append position name to data",
-)
-@click.option(
-    "--format_hcs",
-    required=False,
-    type=bool,
-    help='whether or not to format the data as an HCS "well-plate"',
-)
-def convert2zarr(input, output, data_type, replace_pos_name, format_hcs):
-    """Convert MicroManager ome-tiff to ome-zarr
-
-    Example:
-
-    >> iohub convert -i ./ome-tiff/folder/ -o ./output.zarr
-    """
-    converter = ZarrConverter(
-        input, output, data_type, replace_pos_name, format_hcs
-    )
-    converter.run_conversion()
-
-
-if __name__ == "__main__":
-    cli()
