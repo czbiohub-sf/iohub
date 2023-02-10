a02a948a40516c56f9ab50dad3c368ef44f17c2d
Shalin Mehta <2934183+mattersoflight@users.noreply.github.com>
2023-02-10 09:32:25 -0800

    updated hcs_zarr example

--- examples/hcs_zarr.py
+++ examples/hcs_zarr.py
@@ -8,34 +8,69 @@ import numpy as np
 
 from iohub.ngff import HCSZarr
 
+import os
+
 # %%
 # Write 5D data to multiple wells
 
 position_list = (
     ("A", "1", "0"),
-    ("H", 12, 0),
+    ("H", 10, 0),
     ("Control", "Blank", 0),
 )
 
+file_path = f'{os.path.expanduser("~/")}hcs.zarr'
+
 with HCSZarr.open(
-    "hcs.zarr", mode="a", channel_names=["DAPI", "GFP"]
+    file_path, mode="a", channel_names=["DAPI", "GFP", "Brightfield"]
 ) as dataset:
     for row, col, fov in position_list:
         position = dataset.create_position(row, col, fov)
         position["raw"] = np.random.randint(
             0, np.iinfo(np.uint16).max, size=(5, 3, 2, 32, 32), dtype=np.uint16
         )
+# "raw" is the custom name for the array in the OME-Zarr file.
+# The default name is "0".
+# Dragging above hcs.zarr into napari-ome-zarr doesn't load the array,
+# since the plugin expects the array name to default to "0" .
+# The following zarr will open with each position shown as a tile:
+
+file_path = f'{os.path.expanduser("~/")}hcs_default.zarr'
+
+with HCSZarr.open(
+    file_path, mode="a", channel_names=["DAPI", "GFP", "Brightfield"]
+) as dataset:
+    for row, col, fov in position_list:
+        position = dataset.create_position(row, col, fov)
+        position["0"] = np.random.randint(
+            0, np.iinfo(np.uint16).max, size=(5, 3, 2, 32, 32), dtype=np.uint16
+        )
+
+# Examine the shape, size, dtype, and chunks of the array at a position.
+print(f'Array 0 @ Well A1, FOV 0:\n'
+      f'shape:{dataset["A"]["1"]["0"]["0"].shape}\n'
+      f'size:{dataset["A"]["1"]["0"]["0"].size}\n'
+      f'dtype:{dataset["A"]["1"]["0"]["0"].dtype}\n'
+      f'chunks:{dataset["A"]["1"]["0"]["0"].chunks}\n')
 
 # %%
 # Append a channel to all the positions
 
-with HCSZarr.open("hcs.zarr", mode="r+") as dataset:
+file_path = f'{os.path.expanduser("~/")}hcs_default.zarr'
+
+with HCSZarr.open(file_path, mode="r+") as dataset:
     for name, position in dataset.positions():
         print(name)
-        position.append_channel("New", resize_arrays=True)
-        position[0][:, 2] = np.random.randint(
-            0, np.iinfo(np.uint16).max, size=(5, 3, 32, 32), dtype=np.uint16
+        position.append_channel("Segmentation", resize_arrays=True)
+        position['0'][:, 3] = np.random.randint(
+            0, np.iinfo(np.uint16).max, size=(5, 2, 32, 32), dtype=np.uint16
         )
+
+print(f'Array 0 @ Well A1, FOV 0:\n'
+      f'shape:{dataset["A"]["1"]["0"]["0"].shape}\n'
+      f'size:{dataset["A"]["1"]["0"]["0"].size}\n'
+      f'dtype:{dataset["A"]["1"]["0"]["0"].dtype}\n'
+      f'chunks:{dataset["A"]["1"]["0"]["0"].chunks}\n')
 
 # %%
 # Try viewing the images with napari-ome-zarr
