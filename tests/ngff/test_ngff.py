d62fabdcb05310157c60a9c5690d734c45d22f0f
Ziwen Liu <67518483+ziw-liu@users.noreply.github.com>
2023-02-17 13:36:04 -0800

    test `rename_channel()`

--- tests/ngff/test_ngff.py
+++ tests/ngff/test_ngff.py
@@ -208,6 +208,27 @@ def test_append_channel(channels_and_random_5d, arr_name):
 @given(
     channels_and_random_5d=_channels_and_random_5d(),
     arr_name=short_alpha_numeric,
+    new_channel=short_text_st,
+)
+@settings(
+    max_examples=16,
+    deadline=2000,
+    suppress_health_check=[HealthCheck.data_too_large],
+)
+def test_rename_channel(channels_and_random_5d, arr_name, new_channel):
+    """Test `iohub.ngff.Position.append_channel()`"""
+    channel_names, random_5d = channels_and_random_5d
+    assume(new_channel not in channel_names)
+    with _temp_ome_zarr(random_5d, channel_names, arr_name) as dataset:
+        dataset.rename_channel(old=channel_names[-1], new=new_channel)
+        assert dataset.channel_names == channel_names.append(new_channel)
+        assert len(dataset.metadata.omero.channels) == len(channel_names) + 1
+        assert dataset.metadata.omero.channels[-1].label == new_channel
+
+
+@given(
+    channels_and_random_5d=_channels_and_random_5d(),
+    arr_name=short_alpha_numeric,
 )
 @settings(
     max_examples=16,
