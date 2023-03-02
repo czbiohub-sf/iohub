031d8eda49d6511d8c756f3855034d68a8ef0d05
Ziwen Liu <67518483+ziw-liu@users.noreply.github.com>
2023-03-02 10:48:29 -0800

    test ome-tiff conversion

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,98 +1,55 @@
+import logging
 import os
 import shutil
-import zarr
-from tifffile import TiffFile
+from glob import glob
+from tempfile import TemporaryDirectory
+
 import numpy as np
-from iohub.convert import ZarrConverter
+import zarr
+from tifffile import TiffFile, TiffSequence
+
+from iohub.convert import TIFFConverter
+from iohub.ngff import open_ome_zarr
 from iohub.reader import MicromanagerOmeTiffReader, NDTiffReader
 
 
-def test_ometiff_converter_initialize(
-    setup_data_save_folder, get_ometiff_data_dir
-):
-
-    folder, ometiff_data = get_ometiff_data_dir
-    save_folder = setup_data_save_folder
-
-    input = ometiff_data
-    output = os.path.join(save_folder, "2T_3P_16Z_128Y_256X_Kazansky.zarr")
-
-    if os.path.exists(output):
-        shutil.rmtree(output)
-
-    converter = ZarrConverter(input, output)
-
-    with TiffFile(
-        os.path.join(
-            ometiff_data, "2T_3P_16Z_128Y_256X_Kazansky_1_MMStack_Pos0.ome.tif"
-        )
-    ) as tf:
-        assert (
-            converter.summary_metadata == tf.micromanager_metadata["Summary"]
-        )
-
-    assert converter.dtype == "uint16"
-    assert isinstance(converter.reader, MicromanagerOmeTiffReader)
-    assert isinstance(converter.writer, WaveorderWriter)
-
-    assert converter.dim_order == ["time", "position", "z", "channel"]
-    assert converter.p_dim == 1
-    assert converter.t_dim == 0
-    assert converter.c_dim == 3
-    assert converter.z_dim == 2
-    assert converter.p == 3
-    assert converter.t == 2
-    assert converter.c == 4
-    assert converter.z == 16
-
-
-def test_ometiff_converter_run(setup_data_save_folder, get_ometiff_data_dir):
-
-    folder, ometiff_data = get_ometiff_data_dir
-    save_folder = setup_data_save_folder
-
-    input = ometiff_data
-    output = os.path.join(save_folder, "2T_3P_16Z_128Y_256X_Kazansky.zarr")
-
-    if os.path.exists(output):
-        shutil.rmtree(output)
-
-    converter = ZarrConverter(input, output)
-
-    converter.run_conversion()
-    zs = zarr.open(output, "r")
-
-    assert os.path.exists(
-        os.path.join(
-            save_folder, "2T_3P_16Z_128Y_256X_Kazansky_ImagePlaneMetadata.txt"
-        )
-    )
-
-    cnt = 0
-    for t in range(2):
-        for p in range(3):
-            cnt = t * 16 * 4
-            tf = TiffFile(
-                os.path.join(
-                    ometiff_data,
-                    f"2T_3P_16Z_128Y_256X_Kazansky_1_MMStack_Pos{p}.ome.tif",
-                )
+def test_converter_ometiff(setup_test_data, setup_mm2gamma_ome_tiffs):
+    logging.getLogger("tifffile").setLevel(logging.ERROR)
+    test_data, _, _ = setup_mm2gamma_ome_tiffs
+    for data in os.scandir(test_data):
+        if data.name.startswith("."):
+            # skip system files such as .DS_store
+            continue
+        with TemporaryDirectory() as tmp_dir:
+            output = os.path.join(tmp_dir, "converted.zarr")
+            converter = TIFFConverter(data.path, output)
+            assert isinstance(converter.reader, MicromanagerOmeTiffReader)
+            with TiffSequence(glob(os.path.join(data.path, "*.tif*"))) as ts:
+                raw_array = ts.asarray()
+                with TiffFile(ts[0]) as tf:
+                    assert (
+                        converter.summary_metadata
+                        == tf.micromanager_metadata["Summary"]
+                    )
+            assert converter.dtype == raw_array.dtype
+            assert np.prod([d for d in converter.dim if d > 0]) == np.prod(
+                raw_array.shape
             )
-            for z in range(16):
-                for c in range(4):
-                    image = zs["Row_0"][f"Col_{p}"][f"Pos_00{p}"]["arr_0"][
-                        t, c, z
-                    ]
-                    tiff_image = tf.pages.get(cnt).asarray()
-                    assert np.array_equal(image, tiff_image)
-                    cnt += 1
-            tf.close()
+            assert list(converter.metadata.keys()) == [
+                "iohub_version",
+                "Summary",
+            ]
+            converter.run(check_image=True)
+            with open_ome_zarr(output, mode="r") as result:
+                intensity = 0
+                for _, pos in result.positions():
+                    intensity += pos["0"][:].sum()
+            assert intensity == raw_array.sum()
 
 
 def test_pycromanager_converter_initialize(
     setup_data_save_folder, get_pycromanager_data_dir
 ):
-
     folder, pm_data = get_pycromanager_data_dir
     save_folder = setup_data_save_folder
 
@@ -124,7 +81,6 @@ def test_pycromanager_converter_initialize(
 def test_pycromanager_converter_run(
     setup_data_save_folder, get_pycromanager_data_dir
 ):
-
     folder, pm_data = get_pycromanager_data_dir
     save_folder = setup_data_save_folder
 
