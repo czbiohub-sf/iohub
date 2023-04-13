e30af12ad7334f390961a1fd3a7cf66e71eb130c
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2023-04-13 13:57:06 -0700

    improved _compressed_to_array implementation

--- iohub/clearcontrol.py
+++ iohub/clearcontrol.py
@@ -13,23 +13,43 @@ def _compressed_to_array(
     dtype: np.dtype,
     nthreads: int = 4,
 ) -> np.ndarray:
+    """Loads compressed "blosc" file and converts into numpy array.
+
+    Parameters
+    ----------
+    buffer_path : StrOrBytesPath
+        Compressed blosc buffer path.
+    shape : Tuple[int, ...]
+        Output array shape.
+    dtype : np.dtype
+        Output array data type.
+    nthreads : int, optional
+        Number of blosc decompression threads, by default 4
+
+    Returns
+    -------
+    np.ndarray
+        Output numpy array.
+    """
 
     header_size = 32
     out_arr = np.empty(np.prod(shape), dtype=dtype)
-
-    with open(buffer_path, "rb") as f:
-        compressed_buffer = f.read()
-
     array_buffer = out_arr
 
-    while len(compressed_buffer) > header_size:
-        blosc_header = bytes(compressed_buffer[:header_size])
+    with open(buffer_path, "rb") as f:
+        while True:
+            # read header only
+            blosc_header = bytes(f.read(header_size))
+            if not blosc_header:
+                break
 
-        buffer_size, compressed_buffer_size, _ = blosc2.get_cbuffer_sizes(blosc_header)
-        
-        blosc2.decompress2(compressed_buffer[:compressed_buffer_size], array_buffer, nthreads=nthreads)
+            chunk_size, compress_chunk_size, _ = blosc2.get_cbuffer_sizes(blosc_header)
 
-        array_buffer = array_buffer[buffer_size // out_arr.itemsize:]
-        compressed_buffer = compressed_buffer[compressed_buffer_size:]
+            # move to before the header and read chunk
+            f.seek(f.tell() - header_size)
+            chunk_buffer = f.read(compress_chunk_size)
+
+            blosc2.decompress2(chunk_buffer, array_buffer, nthreads=nthreads)
+            array_buffer = array_buffer[chunk_size // out_arr.itemsize:]
 
     return out_arr.reshape(shape)
