3ffa27ebd45589da0d496f158790bb1e7ee9d991
Ziwen Liu <67518483+ziw-liu@users.noreply.github.com>
2023-05-25 10:44:27 -0700

    fix merge error

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,10 +12,6 @@ import sys
 # If your documentation needs a minimal Sphinx version, state it here.
 # needs_sphinx = '1.0'
 
-# Add any Sphinx extension module names here, as strings. They can be extensions
-# coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-import sys
-
 import importlib_metadata
 
 # If extensions (or modules to document with autodoc) are in another directory,
