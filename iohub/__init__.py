b4ac082e94ba5a4364d85a387cf6e5bb79fe5d07
Ziwen Liu <67518483+ziw-liu@users.noreply.github.com>
2024-02-08 19:50:07 -0800

    set default logging level to INFO

--- iohub/__init__.py
+++ iohub/__init__.py
@@ -6,7 +6,13 @@ N-dimensional bioimaging data I/O with OME metadata in Python
 """
 
 
+import logging
+
 from iohub.ngff import open_ome_zarr
 from iohub.reader import read_images
 
 __all__ = ["open_ome_zarr", "read_images"]
+
+
+logging.basicConfig()
+logging.getLogger(__name__).setLevel(logging.INFO)
