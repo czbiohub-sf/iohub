f93f0f92e5e8816a8d73547c90a3c082dc72ad23
Ziwen Liu <67518483+ziw-liu@users.noreply.github.com>
2024-02-15 11:56:37 -0800

    set log level with an environment variable

--- iohub/__init__.py
+++ iohub/__init__.py
@@ -6,6 +6,7 @@ N-dimensional bioimaging data I/O with OME metadata in Python
 """
 
 import logging
+import os
 
 from iohub.ngff import open_ome_zarr
 from iohub.reader import read_images
@@ -13,5 +14,9 @@ from iohub.reader import read_images
 __all__ = ["open_ome_zarr", "read_images"]
 
 
+_level = os.environ.get("IOHUB_LOG_LEVEL", logging.INFO)
+if _level.isdigit():
+    _level = int(_level)
+
 logging.basicConfig()
-logging.getLogger(__name__).setLevel(logging.INFO)
+logging.getLogger(__name__).setLevel(_level)
