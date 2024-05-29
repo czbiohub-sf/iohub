fa065e62eebba6b13b2440fd185e7b49ac1e1600
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2024-05-28 21:44:25 -0700

    dask implementation

--- iohub/daxi.py
+++ iohub/daxi.py
@@ -1,33 +1,13 @@
-from functools import wraps
 from pathlib import Path
-from typing import TYPE_CHECKING, Any, Callable, Optional, Union, cast
+from typing import TYPE_CHECKING, Any, Optional
 
+import dask
+import dask.array as da
 import numpy as np
 import yaml
 
 if TYPE_CHECKING:
     from _typeshed import StrOrBytesPath
-
-    from iohub.clearcontrol import ArrayIndex
-
-
-def _cached(f: Callable) -> Callable:
-    """Decorator that caches the array data using its key."""
-
-    @wraps(f)
-    def _key_cache_wrapper(
-        self: "DaXiFOV", key: tuple[int | None, ...]
-    ) -> np.ndarray:
-        if not self._cache:
-            return f(self, key)
-
-        elif key != self._cache_key:
-            self._cache_array = f(self, key)
-            self._cache_key = key
-
-        return cast(np.ndarray, self._cache_array)
-
-    return _key_cache_wrapper
 
 
 class DaXiFOV:
@@ -48,8 +28,6 @@ class DaXiFOV:
     missing_value : Optional[int], optional
         If provided this class won't raise an error when missing a volume and
         it will return an array with the provided value.
-    cache : bool
-        When true caches the last array using the first two indices as key.
     """
 
     _CHANNELS_KEY = "Laser wavelengths"
@@ -60,7 +38,6 @@ class DaXiFOV:
         self,
         data_path: "StrOrBytesPath",
         missing_value: Optional[int] = None,
-        cache: bool = False,
     ):
         super().__init__()
 
@@ -71,10 +48,6 @@ class DaXiFOV:
 
         self._missing_value = missing_value
         self._dtype = np.uint16
-        self._cache = cache
-        self._cache_key: tuple[int | None, ...] | None = None
-        self._cache_array: np.ndarray | None = None
-
         self._channels = self._metadata[self._CHANNELS_KEY]
 
         shape_dict = self._metadata[self._SHAPE_KEY]
@@ -86,13 +59,81 @@ class DaXiFOV:
 
         self._shape = tuple(shape_dict[k] for k in self._SHAPE_IDX)
 
+        self._data = da.stack(  # T
+            [
+                da.concatenate(  # V
+                    [
+                        da.stack(  # C
+                            [
+                                da.from_delayed(
+                                    self._load_volume(t, v, c),
+                                    shape=self._shape[2:],
+                                    dtype=self._dtype,
+                                )
+                                for c in range(len(self._channels))
+                            ]
+                        )
+                        for v in range(self._raw_shape[1])
+                    ]
+                )
+                for t in range(self._shape[0])
+            ]
+        )  # T, V * C, Y, X shape
+
+    @dask.delayed
+    def _load_volume(self, t: int, v: int, c: int) -> np.ndarray:
+        """
+        Load a volume from disk.
+        If the volume is missing it returns an array with the missing value.
+
+        Parameters
+        ----------
+        t : int
+            time index.
+        v : int
+            view index.
+        c : int
+            channel index.
+
+        Returns
+        -------
+        np.ndarray
+            Volume array.
+        """
+        path = self._volume_path(t, v)
+
+        if not path.exists():
+            return np.full(
+                self._shape[2:], self._missing_value, dtype=self._dtype
+            )
+
+        return np.memmap(
+            self._volume_path(t, v),
+            dtype=self._dtype,
+            shape=self._raw_shape[2:],
+            mode="r",
+        )[c :: len(self._channels)]
+
     def _volume_path(self, t: int, v: int) -> Path:
+        """
+        Return the path for a volume.
+
+        Parameters
+        ----------
+        t : int
+            time index.
+        v : int
+            view index.
+
+        Returns
+        -------
+        Path
+            Volume path.
+        """
         z, y, x = self._raw_shape[2:]
         volume_path = (
             self._data_path / f"T_{int(t)}.V_{int(v)}.({z}x{y}x{x}).raw"
         )
-        if not volume_path.exists():
-            raise ValueError(f"Volume not found: {volume_path}")
         return volume_path
 
     @property
@@ -107,157 +148,20 @@ class DaXiFOV:
         """Return sorted channels name."""
         return self._channels
 
-    @_cached
-    def _load_array(
-        self, key: tuple[int, int, int | None, int | None]
-    ) -> np.ndarray:
-        """
-        Loads a single or multiple channels from DaXi raw.
-
-        Parameters
-        ----------
-        key : tuple[int, int, int | None, int | None]
-            Time point, view, channel_index, and z-index.
-            z-index and are optional
-
-        Returns
-        -------
-        np.ndarray
-            Volume as an array can be single or multiple channels.
-
-        Raises
-        ------
-        ValueError
-            When expected volume path not found.
-        """
-        shape = list(self._shape[-2:])
-
-        time_point, view, channel, z_index = key
-
-        len_ch = len(self._channels)
-        view //= len_ch
-
-        if channel is None:
-            slicing = slice(None)
-            shape.insert(0, len_ch)
-        else:
-            slicing = slice(channel, None, len_ch)
-            len_ch = 1
-
-        if z_index is None:
-            shape.insert(0, self._shape[2])  # z-shape
-        else:
-            z = z_index * len(self._channels)
-            if channel is not None:
-                slicing = z + channel
-            else:
-                slicing = slice(z, z + len(self._channels))
-
-        map_arr = np.memmap(
-            self._volume_path(time_point, view),
-            dtype=self._dtype,
-            shape=self._raw_shape[2:],
-            mode="r",
-        )
-
-        print("--------")
-        print(key)
-        print(slicing)
-
-        # TODO: catch when volume is not complete
-        arr = map_arr[slicing]
-        print(arr.shape)
-        arr = arr.reshape(shape)
-
-        if len(shape) == 4:
-            # ZCYX -> CZYX
-            arr = arr.swapaxes(0, 1)
-
-        return arr
-
-    @staticmethod
-    def _fix_indexing(indexing: "ArrayIndex", size: int) -> list[int] | int:
-        """Converts numpy array to simple python type or list."""
-        # TODO: check if necessary
-        if isinstance(indexing, slice):
-            return list(range(size)[indexing])
-
-        elif np.isscalar(indexing):
-            try:
-                int_index: int = indexing.item()
-            except AttributeError:
-                int_index = indexing
-
-            if int_index < 0:
-                int_index += size
-
-            return int_index
-
-        elif isinstance(indexing, np.ndarray):
-            return indexing.tolist()
-
-        return indexing
-
-    def _load_array_from_key(
-        self, key: tuple[list[int] | int | None, ...]
-    ) -> np.ndarray:
-        """
-        Load array from a key with multiple channel indices.
-        This function is called recursively until int-only indices are found.
-        """
-        for i, k in enumerate(key):
-            if isinstance(k, int):
-                continue
-
-            elif k is None:
-                if i >= 2:
-                    continue
-
-                k = list(range(self._shape[i]))
-
-            arrs = []
-            for int_key in k:
-                new_key = key[:i] + (int_key,) + key[i + 1 :]
-                arrs.append(self._load_array_from_key(new_key))
-
-            return np.stack(arrs)
-
-        return self._load_array(key)
-
-    def __getitem__(
-        self, key: Union["ArrayIndex", tuple["ArrayIndex", ...]]
-    ) -> np.ndarray:
+    def __getitem__(self, index) -> da.Array:
         """Lazily load array as indexed.
 
         Parameters
         ----------
-        key : ArrayIndex | tuple[ArrayIndex, ...]
+        index : Array index.
             An indexing key as in numpy, but a bit more limited.
 
         Returns
         -------
         np.ndarray
             Output array.
-
-        Raises
-        ------
-        NotImplementedError
-            Not all numpy array of indexing are implemented.
         """
-        # standardizing indexing
-        yx_slicing = slice(None)
-        min_size = 4
-
-        if not isinstance(key, tuple):
-            key = (key,)
-
-        key = tuple(self._fix_indexing(k, s) for s, k in zip(self._shape, key))
-        args_key = key + (None,) * (min_size - len(key))
-
-        if len(args_key) > min_size:  # min_size + 1 (z)
-            args_key, yx_slicing = args_key[:min_size], args_key[min_size:]
-
-        return self._load_array_from_key(args_key)[yx_slicing]
+        return self._data[index]
 
     def __setitem__(self, key: Any, value: Any) -> None:
         raise PermissionError("DaXiFOV is read-only.")
@@ -270,18 +174,6 @@ class DaXiFOV:
     def dtype(self) -> np.dtype:
         return self._dtype
 
-    @property
-    def cache(self) -> bool:
-        return self._cache
-
-    @cache.setter
-    def cache(self, value: bool) -> None:
-        """Free current key/array cache."""
-        self._cache = value
-        if not value:
-            self._cache_array = None
-            self._cache_key = None
-
     def metadata(self) -> dict[str, Any]:
         """Summarizes Clear Control metadata into a dictionary."""
         return self._metadata
@@ -289,7 +181,7 @@ class DaXiFOV:
     @property
     def scale(self) -> list[float]:
         """Dataset temporal, channel and spacial scales."""
-        # TODO: use metadata
+        # TODO: use actual metadata, information is missing from file format
         return [
             1.0,
             1.0,
@@ -317,7 +209,7 @@ def create_mock_daxi_dataset(path: "StrOrBytesPath") -> None:
     )
 
     metadata = {
-        "Laser wavelenghts": ["488", "561"],
+        "Laser wavelengths": ["488", "561"],
         "Dataset dimension": {
             "T": 2,
             "V": 2,
@@ -337,13 +229,3 @@ def create_mock_daxi_dataset(path: "StrOrBytesPath") -> None:
             )
             raw_map[...] = array[t, v]
             raw_map.flush()
-
-
-if __name__ == "__main__":
-    import napari
-
-    path = Path("/mnt/royer.daxi2/Merlin/neurog1.h2afva_05_21_2024")
-    ds = DaXiFOV(path, cache=True)
-
-    napari.imshow(ds)
-    napari.run()
--- tests/daxi/test_daxi.py
+++ tests/daxi/test_daxi.py
@@ -3,8 +3,8 @@ from pathlib import Path
 import numpy as np
 import pytest
 
-from iohub.daxi import create_mock_daxi_dataset, DaXiFOV
 from iohub.clearcontrol import ArrayIndex
+from iohub.daxi import DaXiFOV, create_mock_daxi_dataset
 
 
 @pytest.fixture
@@ -16,14 +16,14 @@ def mock_daxi_dataset_path(tmp_path: Path) -> Path:
 
 @pytest.mark.parametrize(
     "key",
-    [1,
-     (slice(None), 1),
-     (0, [1, 2]),
-     (-1, np.asarray([0, 3])),
-     (slice(1), -2),
-     (np.asarray(0),),
-     (0, 1, 57, slice(32), slice(32, None)),
-     (0, 0, slice(32)),
+    [
+        1,
+        (slice(None), 1),
+        (0, [1, 2]),
+        (-1, np.asarray([0, 3])),
+        (slice(1), -2),
+        (0, 1, 57, slice(32), slice(32, None)),
+        (0, 0, slice(32)),
     ],
 )
 def test_DaXiFOV_indexing(
@@ -33,30 +33,7 @@ def test_DaXiFOV_indexing(
     fov = DaXiFOV(mock_daxi_dataset_path)
 
     # copy of whole array
-    similar_arr = fov[slice(None)]
+    similar_arr = np.asarray(fov[...])
 
     # checking if indexing works as in numpy
-    assert np.array_equal(similar_arr[key], fov[key])
-
-
-def test_DaXiFOV_cache(
-    mock_daxi_dataset_path: Path,
-) -> None:
-    fov = DaXiFOV(mock_daxi_dataset_path, cache=True)
-
-    volume = fov[0, 0]
-    assert np.array_equal(fov._cache_array, volume)
-
-    fov[0, 0, 0]
-    assert np.array_equal(fov._cache_array, volume)
-    assert fov._cache_key == (0, 0)
-
-    array = fov[1]
-    assert np.array_equal(fov._cache_array, array)
-
-    new_array = fov[1]
-    assert id(new_array) == id(array)   # same reference, so cache worked
-
-    fov.cache = False
-    assert fov._cache_key is None
-    assert fov._cache_array is None
+    assert np.array_equal(similar_arr[key], np.asarray(fov[key]))
