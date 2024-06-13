ed095548cc0561548cc0fe35dc608081890dc5ab
Ziwen Liu <67518483+ziw-liu@users.noreply.github.com>
2024-06-13 10:10:05 -0700

    add Y5 to orange display color group

--- iohub/display_utils.py
+++ iohub/display_utils.py
@@ -26,7 +26,7 @@ CHANNEL_COLORS = {
     "blue": ["Blue", "DAPI", "BFP", "Hoechst"],
     "red": ["Red"],
     "yellow": ["Yellow", "Cy3"],  # Emission around 540-570 nm
-    "orange": ["Orange", "Cy5"],  # emission around 650-680 nm
+    "orange": ["Orange", "Cy5", "Y5"],  # emission around 650-680 nm
 }
 
 
