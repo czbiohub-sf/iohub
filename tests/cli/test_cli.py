b49980e21770850f0af2af7743930a5e83d5cd3c
Talon Chandler <talonchandler@gmail.com>
2024-09-25 19:58:19 -0700

    test old_* metadata

--- tests/cli/test_cli.py
+++ tests/cli/test_cli.py
@@ -129,7 +129,6 @@ def test_cli_set_scale():
                 "set-scale",
                 "-i",
                 str(position_path),
-                str(position_path),
                 "-z",
                 random_z,
                 "-y",
@@ -144,6 +143,9 @@ def test_cli_set_scale():
         with open_ome_zarr(position_path, layout="fov") as output_dataset:
             assert tuple(output_dataset.scale[-3:]) == (random_z, 0.5, 0.5)
             assert output_dataset.scale != old_scale
+            assert output_dataset.zattrs["iohub"]["old_x"] == old_scale[-1]
+            assert output_dataset.zattrs["iohub"]["old_y"] == old_scale[-2]
+            assert output_dataset.zattrs["iohub"]["old_z"] == old_scale[-3]
 
         # Test plate-expands-into-positions behavior
         runner = CliRunner()
@@ -159,3 +161,4 @@ def test_cli_set_scale():
         )
         with open_ome_zarr(position_path, layout="fov") as output_dataset:
             assert output_dataset.scale[-1] == 0.1
+            assert output_dataset.zattrs["iohub"]["old_x"] == 0.5
