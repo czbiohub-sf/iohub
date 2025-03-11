99b9afbbf6435b968a7aef3d0be6c7e1afb755a4
Ziwen Liu <67518483+ziw-liu@users.noreply.github.com>
2025-03-10 17:02:42 -0700

    fix kwarg name

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,7 +17,7 @@ def _download_ndtiff_v3_labeled_positions(test_data: Path) -> None:
         "github",
         org="micro-manager",
         repo="NDTiffStorage",
-        user=user,
+        username=user,
         token=token,
     )
     v3_lp = test_data / "ndtiff_v3_labeled_positions"
